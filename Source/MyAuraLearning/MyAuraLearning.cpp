05e86bf90348561141b9553eb589ccbfa0133c22
NoFire <nofire@foxmail.com>
2025-02-07 01:53:53 +0800

    First commit: Init project

---
+++ Source/MyAuraLearning/MyAuraLearning.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MyAuraLearning.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyAuraLearning, "MyAuraLearning" );
